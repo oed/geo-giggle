a3909de359b47ae3025e54908c6d39e36ed78771
Radek Sienkiewicz <mail@velvetshark.com>
2023-09-26 16:00:08 -0600

    Favicon

--- src/components/Map/DynamicMap.js
+++ src/components/Map/DynamicMap.js
@@ -1,16 +1,35 @@
-import { useEffect } from 'react';
-import Leaflet from 'leaflet';
-import * as ReactLeaflet from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from "react";
+import Leaflet from "leaflet";
+import * as ReactLeaflet from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-import styles from './Map.module.scss';
+import styles from "./Map.module.scss";
 
-const { MapContainer } = ReactLeaflet;
+const { MapContainer, useMapEvents } = ReactLeaflet;
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
 
 const Map = ({ children, className, width, height, ...rest }) => {
   let mapClassName = styles.map;
 
-  if ( className ) {
+  if (className) {
     mapClassName = `${mapClassName} ${className}`;
   }
 
@@ -18,9 +37,9 @@ const Map = ({ children, className, width, height, ...rest }) => {
     (async function init() {
       delete Leaflet.Icon.Default.prototype._getIconUrl;
       Leaflet.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'leaflet/images/marker-icon-2x.png',
-        iconUrl: 'leaflet/images/marker-icon.png',
-        shadowUrl: 'leaflet/images/marker-shadow.png',
+        iconRetinaUrl: "leaflet/images/marker-icon-2x.png",
+        iconUrl: "leaflet/images/marker-icon.png",
+        shadowUrl: "leaflet/images/marker-shadow.png",
       });
     })();
   }, []);
@@ -28,8 +47,9 @@ const Map = ({ children, className, width, height, ...rest }) => {
   return (
     <MapContainer className={mapClassName} {...rest}>
       {children(ReactLeaflet, Leaflet)}
+      <LocationMarker />
     </MapContainer>
-  )
-}
+  );
+};
 
 export default Map;
