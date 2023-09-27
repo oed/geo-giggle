4c3e5663183775af45b8c29c890f39173f0f631b
Radek Sienkiewicz <mail@velvetshark.com>
2023-09-27 17:09:05 -0600

    Modal component + showing leaderboard

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,22 @@
+import React from "react";
+
 import Container from "@components/Container";
+import GeoJiggleModal from "@components/Modal";
+import Button from "react-bootstrap/Button";
 
 import styles from "./Footer.module.scss";
 
 const Footer = ({ ...rest }) => {
+  const [modalShow, setModalShow] = React.useState(false);
   return (
     <footer className={styles.footer} {...rest}>
       <Container className={`${styles.footerContainer} ${styles.footerLegal}`}>
-        <p>&copy; GeoJiggle {new Date().getFullYear()}</p>
+        <GeoJiggleModal />
+        <Button variant='primary' onClick={() => setModalShow(true)}>
+          Leaderboard
+        </Button>
+
+        <GeoJiggleModal title='Leaderboard' show={modalShow} onHide={() => setModalShow(false)} />
       </Container>
     </footer>
   );
