714482793ce02f52ebc1477d8103a311e3e90dca
Radek Sienkiewicz <mail@velvetshark.com>
2023-09-27 19:05:32 -0600

    Leaderboard address presentation, links to Etherscan

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,10 +1,45 @@
 import { useState, useEffect } from "react";
 import { useComposeDB } from "../../hooks/useComposeDB";
+import { useEnsName } from "wagmi";
+import Link from "next/link";
+
+import styles from "./Leaderboard.module.css";
+
+function Leader({ author, count }) {
+  const { data: ensName, isError, isLoading } = useEnsName({ address: author.split(":")[4] });
+
+  if (isError) {
+    console.error("Error fetching ENS name for", author, ":", isError);
+  }
+
+  if (isLoading) {
+    console.log("Loading ENS name for", author, "...");
+  }
+
+  function formatAddress(address) {
+    return address.split(":")[4];
+    // return `${stripped.slice(0, 6)}...${stripped.slice(-4)}`;
+  }
+
+  return (
+    <li>
+      <strong>
+        [{count} pin{count > 1 && "s"}]
+      </strong>
+      &nbsp;-&nbsp;
+      <Link href={`https://etherscan.io/address/${formatAddress(author)}`}>
+        <span className={styles.walletAddress}>
+          {ensName || `${formatAddress(author).slice(0, 6)}...${formatAddress(author).slice(-4)}`}
+        </span>
+      </Link>
+    </li>
+  );
+}
 
 export default function Leaderboard() {
   const { compose } = useComposeDB();
-
   const [leaders, setLeaders] = useState([]);
+
   async function loadLeaders() {
     const pins = await compose.executeQuery(`
     query {
@@ -37,22 +72,14 @@ export default function Leaderboard() {
     );
   }
 
-  function getLeaders() {
-    useEffect(() => {
-      loadLeaders();
-    }, []);
-
-    return leaders;
-  }
-
-  const leaderComponents = getLeaders();
+  useEffect(() => {
+    loadLeaders();
+  }, []);
 
   return (
     <ol>
-      {leaderComponents.map((leader) => (
-        <li>
-          <strong>{leader.count}</strong> {leader.author}
-        </li>
+      {leaders.map((leader, index) => (
+        <Leader key={leader.author} {...leader} />
       ))}
     </ol>
   );
