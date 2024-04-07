1dc4dcb90977a4ea59fc32ed2e59df4c04996e47
Radek Sienkiewicz <mail@velvetshark.com>
2024-04-07 17:38:44 -0400

    Change default location to Montreal

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,23 +11,25 @@ import styles from "@styles/Home.module.scss";
 
 import { useComposeDB } from "../hooks/useComposeDB";
 
-const DEFAULT_CENTER = [19.413894958323255, -99.17421357377354];
+// const DEFAULT_CENTER = [19.413894958323255, -99.17421357377354]; // Mexico City location
+const DEFAULT_CENTER = [45.506602566939996, -73.57133840228119]; // Montreal location
 const DESCRIPTION =
   "GeoJiggle is a user-friendly, decentralized platform for collaborative and interactive mapping experiences.";
 
 export default function Home() {
   const { compose, isAuthenticated } = useComposeDB();
   const [tags, setTags] = useState({
-    "danger": true,
-    "interest": true,
-    "food": true,
+    danger: true,
+    interest: true,
+    food: true
   });
 
   const [pins, setPins] = useState([]);
   async function loadPins() {
-    const tagList = Object.keys(tags).flatMap((k) => tags[k] ? [k] : []);
+    const tagList = Object.keys(tags).flatMap((k) => (tags[k] ? [k] : []));
     const input = { where: { tag: { in: tagList } } };
-    const pins = await compose.executeQuery(`
+    const pins = await compose.executeQuery(
+      `
         query($input: PinFiltersInput) {
           pinIndex(first:100, filters: $input) {
             edges {
@@ -42,31 +44,33 @@ export default function Home() {
               }
             }
           }
-      }`, { input });
+      }`,
+      { input }
+    );
     if (pins.data && pins.data.pinIndex) {
       setPins(pins.data.pinIndex.edges.map((edge) => edge.node));
     } else {
-      setPins([])
+      setPins([]);
     }
   }
 
-  const [loc, setLoc] = useState(null)
+  const [loc, setLoc] = useState(null);
   async function loadLocation() {
-    const { coords: { latitude, longitude } } = await new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-    setLoc([latitude, longitude])
+    const {
+      coords: { latitude, longitude }
+    } = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    setLoc([latitude, longitude]);
   }
 
+  useEffect(() => {
+    loadPins();
+  }, [tags]);
 
   useEffect(() => {
-    loadPins()
-  }, [tags])
-
-  useEffect(() => {
-    loadLocation()
-  }, [])
-
+    loadLocation();
+  }, []);
 
   const [newMarker, setNewMarker] = useState(false);
 
@@ -74,40 +78,45 @@ export default function Home() {
     <Layout newMarker={newMarker} setNewMarker={setNewMarker} tags={tags} setTags={setTags}>
       <Head>
         <title>GeoJiggle</title>
-        <meta name='description' content={DESCRIPTION} />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content={DESCRIPTION} />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Section>
         <Container>
-          <Map className={styles.homeMap} center={loc || DEFAULT_CENTER} zoom={15} loadPins={loadPins} newMarker={newMarker} setNewMarker={setNewMarker}>
+          <Map
+            className={styles.homeMap}
+            center={loc || DEFAULT_CENTER}
+            zoom={15}
+            loadPins={loadPins}
+            newMarker={newMarker}
+            setNewMarker={setNewMarker}
+          >
             {({ TileLayer, Marker, Popup, CircleMarker }, { Icon }) => (
               <>
                 <TileLayer
-                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {loc && <CircleMarker center={loc} radius={4} />}
-                {
-                  pins.map((pin) => {
-                    const colorMod = (new TextEncoder()).encode(pin.tag).join('')
-                    const icon = new Icon.Default({ className: `color-${colorMod}` })
+                {pins.map((pin) => {
+                  const colorMod = new TextEncoder().encode(pin.tag).join("");
+                  const icon = new Icon.Default({ className: `color-${colorMod}` });
 
-                    const styleSheet = document.createElement("style")
-                    styleSheet.innerText += `
-                      .color-${colorMod} { filter: hue-rotate(${colorMod%999}deg); }
-                    `
-                    document.head.appendChild(styleSheet)
+                  const styleSheet = document.createElement("style");
+                  styleSheet.innerText += `
+                      .color-${colorMod} { filter: hue-rotate(${colorMod % 999}deg); }
+                    `;
+                  document.head.appendChild(styleSheet);
 
-                    return (
-                      <Marker position={[pin.lat, pin.lon]} key={pin.id} icon={icon} style="234">
-                        <Popup>
-                          <PinContent pin={pin} />
-                        </Popup>
-                      </Marker>
-                    )
-                  })
-                }
+                  return (
+                    <Marker position={[pin.lat, pin.lon]} key={pin.id} icon={icon} style="234">
+                      <Popup>
+                        <PinContent pin={pin} />
+                      </Popup>
+                    </Marker>
+                  );
+                })}
               </>
             )}
           </Map>
@@ -116,4 +125,3 @@ export default function Home() {
     </Layout>
   );
 }
-
